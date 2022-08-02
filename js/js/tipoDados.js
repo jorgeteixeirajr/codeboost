c3a51961575d9068f6f684b8c11f7e76782efa77
Jorge Teixeira Jr <jorgesergio.teixeira@gmail.com>
2022-08-01 21:23:17 -0300

    created tipoDados.js

---
+++ js/js/tipoDados.js
@@ -0,0 +1,24 @@
+let nome = "Jorge"; // string
+let idade = 27; // number
+const brasileiro = true; // boolean
+var nacionalidade; // undefined
+var religiao = null; // object
+var informacoesBiblia = {
+  livros: 66,
+  versiculos: 12053,
+  versiculoAtual: 1,
+}; // object
+var infoLivro = [
+  {
+    nome: "Gênesis",
+    versiculos: 50,
+    autor: "Moisés",
+  },
+  {
+    nome: "Êxodo",
+    versiculos: 40,
+    autor: "Moisés",
+  },
+]; //object
+
+console.log(typeof nacionalidade);
