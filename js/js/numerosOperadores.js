67752327e3d1c2613fb9fdd527e9372b64616695
Jorge Teixeira Jr <jorgesergio.teixeira@gmail.com>
2022-08-02 21:19:17 -0300

    inclusion of numbers

---
+++ js/js/numerosOperadores.js
@@ -0,0 +1,8 @@
+let idade = 29;
+let anoNascimento = 1994;
+let mesNascimento = 08;
+let diaNascimento = 22;
+
+console.log(
+  `O Jorge tem ${idade} anos, nasceu no dia ${diaNascimento} de ${mesNascimento} de ${anoNascimento}`
+);
