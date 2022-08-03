78faa4b0fa89eb61dcd70cb789dfe1ac3e557169
Jorge Teixeira Jr <jorgesergio.teixeira@gmail.com>
2022-08-03 20:43:30 -0300

    inclusion of functions

---
+++ js/js/funcoes.js
@@ -0,0 +1,11 @@
+function carroFavorito(carro) {
+  if (carro === "Fusca") {
+    console.log("Fusca é o carro favorito");
+  } else if (carro === "Gol") {
+    console.log("Gol é o carro favorito");
+  } else {
+    console.log("Outro carro é o carro favorito");
+  }
+}
+
+carroFavorito();
