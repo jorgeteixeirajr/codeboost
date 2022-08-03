4a25cc65086ef53e529a72c1ac05348b0083187f
Jorge Teixeira Jr <jorgesergio.teixeira@gmail.com>
2022-08-02 21:24:28 -0300

    inclusion of operators

--- js/js/numerosOperadores.js
+++ js/js/numerosOperadores.js
@@ -6,3 +6,11 @@ let diaNascimento = 22;
 console.log(
   `O Jorge tem ${idade} anos, nasceu no dia ${diaNascimento} de ${mesNascimento} de ${anoNascimento}`
 );
+
+let soma = idade + anoNascimento;
+let subtracao = idade - anoNascimento;
+let multiplicacao = idade * anoNascimento;
+let divisao = idade / anoNascimento;
+let expoente = idade ** anoNascimento;
+
+let notNumber = diaNascimento / 5; // NaN (Not a Number)
