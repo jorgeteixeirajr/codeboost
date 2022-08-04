1314dd9db2b82a890a8f07d7d68caf0df202b5de
Jorge Teixeira Jr <jorgesergio.teixeira@gmail.com>
2022-08-03 21:14:32 -0300

    ✨ inclusion of arrays and loops

---
+++ js/js/arraysLoops.js
@@ -0,0 +1,29 @@
+// Arrays
+
+var livros = ["Genesis", "Exodo", "Leviticus", "Numeros", "Deuteronomio"];
+
+console.log(livros[2]);
+
+livros.pop(); // remove o ultimo elemento do array
+livros.push("romanos"); // adiciona um elemento no final do array
+livros.length; // retorna o tamanho do array
+
+// For loop
+
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+for (var i = 0; i < livros.length; i++) {
+  console.log(livros[i]);
+
+  if (livros[i] === "Genesis") {
+    break;
+  }
+}
+
+// For Each
+
+// item
+// index
+// array
