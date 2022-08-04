d4fff17d104d3b79588f3e624348167562adca76
Jorge Teixeira Jr <jorgesergio.teixeira@gmail.com>
2022-08-03 21:21:27 -0300

    ⚡️ correction in code

--- js/js/arraysLoops.js
+++ js/js/arraysLoops.js
@@ -5,14 +5,14 @@ var livros = ["Genesis", "Exodo", "Leviticus", "Numeros", "Deuteronomio"];
 console.log(livros[2]);
 
 livros.pop(); // remove o ultimo elemento do array
-livros.push("romanos"); // adiciona um elemento no final do array
+livros.push("Romanos"); // adiciona um elemento no final do array
 livros.length; // retorna o tamanho do array
 
 // For loop
 
-// for (var i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
 
 for (var i = 0; i < livros.length; i++) {
   console.log(livros[i]);
