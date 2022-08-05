dfdaccb684312b1ae6cecb80dd5316ca570d1385
Jorge Teixeira Jr <jorgesergio.teixeira@gmail.com>
2022-08-05 18:02:53 -0300

    ✨ include querySelector and getElementById

---
+++ js/js/selecionandoElementosDOM.js
@@ -0,0 +1,5 @@
+const genesis = document.getElementById("js-genesis"); // Seleciona o elemento com id js-genesis
+
+const livros = document.querySelector(".livros"); //  Seleciona o primeiro elemento com classe livros
+
+const titulo = document.querySelectorAll(".titulo"); // Seleciona todos os elementos com classe titulo
